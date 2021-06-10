560d6926c6a87f925654c1c6e9063afd6f52a0e3
joaopaulo1094@gmail.com <joaopaulo1094@gmail.com>
2021-06-10 10:23:43 -0300

    Primeiro Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const App = ({ Component, pageProps }) => {
+    useEffect(() => {
+        document.documentElement.lang = 'pt-br'
+    })
+
+    return (
+        <>
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#000000" />
+                <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
-export default MyApp
+export default App
